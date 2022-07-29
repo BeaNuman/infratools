b8d66757607a73c86ed66f95085e7756d57010e8
Andy Paine <andy.paine@numan.com>
2022-07-29 09:18:05 +0100

    Use AppRole authentication rather than direct tokens
    Making long lived tokens can be risky so we are going to use AppRole
    auth instead and then fetch a token when we need it.
    
    Splitting the functions apart makes this a bit easier to test/mock and
    generally makes the script more readable

--- scripts/fetch-vault-credentials_test.py
+++ scripts/fetch-vault-credentials_test.py
@@ -1,21 +1,55 @@
 import os
 from unittest.mock import MagicMock, patch
+from urllib.error import HTTPError
 import pytest
 
 import fetch_vault_credentials
 
 VAULT_ENV_VARS = {
-    "VAULT_DEPLOYMENT_TOKEN": "abc123",
+    "VAULT_ROLE_NAMESPACE": "namespace",
+    "VAULT_ROLE_ID": "abc-123",
+    "VAULT_ROLE_SECRET": "def-456",
     "VAULT_ADDRESS": "https://vault.test:1000",
-    "VAULT_NAMESPACE": "ns",
+    "VAULT_NAMESPACE": "namespace/nested",
     "VAULT_KV_STORE": "kv",
     "VAULT_SECRET": "secret",
 }
 
-def secret_mock():
-    cm = MagicMock()
-    cm.getcode.return_value = 200
-    cm.read.return_value = '''{
+@pytest.mark.parametrize("omit_env_var", VAULT_ENV_VARS.keys())
+def test_env_vars_are_required(omit_env_var):
+    with patch.dict(os.environ, {k: v for k, v in VAULT_ENV_VARS.items() if k != omit_env_var}, clear=True):
+        with pytest.raises(OSError, match=f"The environment variable {omit_env_var} must be set."):
+            fetch_vault_credentials.main()
+
+@patch('urllib.request.urlopen')
+def test_token_fetched_correctly(mock_urlopen):
+    token_mock = MagicMock()
+    token_mock.read.return_value = '{ "auth": { "client_token": "xyz-789" } }'.encode()
+    mock_urlopen.return_value = token_mock
+
+    token = fetch_vault_credentials.fetch_token("https://vault.test:1000", "namespace", "abc-123", "def-345")
+    assert token == "xyz-789"
+
+@patch('urllib.request.urlopen')
+def test_token_fetching_fails(mock_urlopen):
+    mock_urlopen.side_effect = HTTPError('https://vault.test:1000/v1/auth/approle/login', 403, 'Unauthorized', {}, None)
+
+    with pytest.raises(Exception, match="Error fetching Vault AppRole token: HTTP Error 403: Unauthorized"):
+        fetch_vault_credentials.fetch_token("https://vault.test:1000", "namespace", "abc-123", "def-345")
+
+@patch('urllib.request.urlopen')
+def test_token_json_decoding_fails(mock_urlopen):
+    token_mock = MagicMock()
+    token_mock.read.return_value = '{ "auth": { "client_token": "xyz-789" }'.encode()
+    mock_urlopen.return_value = token_mock
+
+    with pytest.raises(Exception, match="Error decoding Vault AppRole token from response"):
+        fetch_vault_credentials.fetch_token("https://vault.test:1000", "namespace", "abc-123", "def-345")
+
+@patch('urllib.request')
+def test_secret_fetched_correctly(mock_request):
+    secret_mock = MagicMock()
+    secret_mock.read.return_value = '''{
         "data": {
             "data": {
                 "KEY1": "VALUE1",
@@ -23,19 +57,40 @@ def secret_mock():
             }
         }
     }'''.encode()
-    return cm
+    mock_request.urlopen.return_value = secret_mock
 
-@pytest.mark.parametrize("omit_env_var", ["VAULT_DEPLOYMENT_TOKEN", "VAULT_ADDRESS", "VAULT_NAMESPACE", "VAULT_KV_STORE", "VAULT_SECRET"])
-def test_env_vars_are_required(omit_env_var):
-    with patch.dict(os.environ, {k: v for k, v in VAULT_ENV_VARS.items() if k != omit_env_var}, clear=True):
-        with pytest.raises(OSError, match=f"The environment variable {omit_env_var} must be set."):
-            fetch_vault_credentials.main()
+    with patch.dict(os.environ, VAULT_ENV_VARS):
+        secret = fetch_vault_credentials.fetch_secret("https://vault.test:1000", "xyz-789", "namespace", "kv", "secret")
 
-@patch('urllib.request')
+    assert secret == {'KEY1': 'VALUE1', 'KEY2': 'VALUE2'}
+
+@patch('urllib.request.urlopen')
+def test_secret_fetching_fails(mock_urlopen):
+    mock_urlopen.side_effect = HTTPError('https://vault.test:1000/v1/kv/data/secret', 403, 'Unauthorized', {}, None)
+
+    with pytest.raises(Exception, match="Error fetching Vault secret: HTTP Error 403: Unauthorized"):
+        fetch_vault_credentials.fetch_secret("https://vault.test:1000", "xyz-789", "namespace", "kv", "secret")
+
+@patch('urllib.request.urlopen')
+def test_secret_json_decoding_fails(mock_urlopen):
+    secret_mock = MagicMock()
+    secret_mock.read.return_value = '''{
+        "data": {
+            "data": {
+                "KEY1": "VALUE1",
+                "KEY2": "VALUE2"
+            }
+    }'''.encode()
+    mock_urlopen.return_value = secret_mock
+
+    with pytest.raises(Exception, match="Error decoding Vault secret from response"):
+        fetch_vault_credentials.fetch_secret("https://vault.test:1000", "xyz-789", "namespace", "kv", "secret")
+
+@patch('fetch_vault_credentials.fetch_token', return_value="xyz-789")
+@patch('fetch_vault_credentials.fetch_secret', return_value={'KEY1': 'VALUE1', 'KEY2': 'VALUE2'})
 @patch('builtins.print')
 @patch('sys.stdout')
-def test_secret_fetched_correctly(mock_stdout, mock_print, mock_request):
-    mock_request.urlopen.return_value = secret_mock()
+def test_secret_is_printed_when_stdout_is_not_a_tty(mock_stdout, mock_print, _, __):
     mock_stdout.isatty.return_value = False
 
     with patch.dict(os.environ, VAULT_ENV_VARS):
@@ -43,21 +98,11 @@ def test_secret_fetched_correctly(mock_stdout, mock_print, mock_request):
 
     mock_print.assert_called_once_with("KEY1=VALUE1\nKEY2=VALUE2")
 
-@patch('urllib.request')
-def test_secret_fetching_failed(mock_request):
-    cm = MagicMock()
-    cm.getcode.return_value = 403
-    mock_request.urlopen.return_value = cm
-
-    with patch.dict(os.environ, VAULT_ENV_VARS):
-        with pytest.raises(Exception, match=f"Request to Vault failed with status code 403"):
-            fetch_vault_credentials.main()
-
-@patch('urllib.request')
+@patch('fetch_vault_credentials.fetch_token', return_value="xyz-789")
+@patch('fetch_vault_credentials.fetch_secret', return_value={'KEY1': 'VALUE1', 'KEY2': 'VALUE2'})
 @patch('builtins.print')
 @patch('sys.stdout')
-def test_does_not_write_if_stdout_is_a_tty(mock_stdout, mock_print, mock_request):
-    mock_request.urlopen.return_value = secret_mock()
+def test_secret_is_not_printed_when_stdout_is_a_tty(mock_stdout, mock_print, _, __):
     mock_stdout.isatty.return_value = True
 
     with patch.dict(os.environ, VAULT_ENV_VARS):
--- scripts/fetch_vault_credentials.py
+++ scripts/fetch_vault_credentials.py
@@ -1,62 +1,68 @@
 #!/usr/bin/env python3
 """
-Launch a process with an environment partially populated from Vault.
+Fetch secrets from Vault for source-ing into the application environment
 
 This script connects to a Vault instance via its HTTP API and attempts
 to pull sensitive environment variables from a namespace as specified by
-the command line arguments. A process is then spawned with the sensitive
-environment variables set.
+the VAULT_* environment variables.
 """
 import json
 import os
 import sys
+from urllib.error import HTTPError
 
 import urllib.request
 
 def main():
-    if "VAULT_DEPLOYMENT_TOKEN" not in os.environ:
-        raise OSError("The environment variable VAULT_DEPLOYMENT_TOKEN must be set.")
-    if "VAULT_ADDRESS" not in os.environ:
-        raise OSError("The environment variable VAULT_ADDRESS must be set.")
-    if "VAULT_NAMESPACE" not in os.environ:
-        raise OSError("The environment variable VAULT_NAMESPACE must be set.")
-    if "VAULT_KV_STORE" not in os.environ:
-        raise OSError("The environment variable VAULT_KV_STORE must be set.")
-    if "VAULT_SECRET" not in os.environ:
-        raise OSError("The environment variable VAULT_SECRET must be set.")
+    for env_var in ["VAULT_ROLE_NAMESPACE", "VAULT_ROLE_ID", "VAULT_ROLE_SECRET", "VAULT_ADDRESS", "VAULT_NAMESPACE", "VAULT_KV_STORE", "VAULT_SECRET"]:
+        if env_var not in os.environ:
+            raise OSError(f"The environment variable {env_var} must be set.")
 
-    VAULT_DEPLOYMENT_TOKEN = os.getenv("VAULT_DEPLOYMENT_TOKEN")
+    VAULT_ROLE_NAMESPACE = os.getenv("VAULT_ROLE_NAMESPACE")
+    VAULT_ROLE_ID = os.getenv("VAULT_ROLE_ID")
+    VAULT_ROLE_SECRET = os.getenv("VAULT_ROLE_SECRET")
     VAULT_ADDRESS = os.getenv("VAULT_ADDRESS")
     VAULT_NAMESPACE = os.getenv("VAULT_NAMESPACE")
     VAULT_KV_STORE = os.getenv("VAULT_KV_STORE")
     VAULT_SECRET = os.getenv("VAULT_SECRET")
 
+    vault_token = fetch_token(VAULT_ADDRESS, VAULT_ROLE_NAMESPACE, VAULT_ROLE_ID, VAULT_ROLE_SECRET)
+    secret = fetch_secret(VAULT_ADDRESS, vault_token, VAULT_NAMESPACE, VAULT_KV_STORE, VAULT_SECRET)
 
+    if not sys.stdout.isatty():
+        print('\n'.join(f"{key}={value}" for key, value in secret.items()))
+
+def fetch_token(vault_address, role_namespace, role_id, role_secret):
+    try:
+        req = urllib.request.Request(
+            method='POST',
+            url=f"{vault_address}/v1/auth/approle/login",
+            headers={"X-Vault-Namespace": role_namespace},
+            data=json.dumps({'role_id': role_id, 'secret_id': role_secret}).encode('utf-8')
+        )
+        vault_token_resp = urllib.request.urlopen(req).read()
+        return json.loads(vault_token_resp.decode("utf-8"))["auth"]["client_token"]
+    except json.JSONDecodeError as e:
+        raise Exception("Error decoding Vault AppRole token from response")
+    except Exception as e:
+        raise Exception(f"Error fetching Vault AppRole token: {e}")
+
+def fetch_secret(vault_address, vault_token, namespace, kv_store, secret):
     try:
         req = urllib.request.Request(
             method='GET',
-            url=f"{VAULT_ADDRESS}/v1/{VAULT_KV_STORE}/data/{VAULT_SECRET}",
+            url=f"{vault_address}/v1/{kv_store}/data/{secret}",
             headers={
-                "X-Vault-Namespace": VAULT_NAMESPACE,
-                "X-Vault-Token": VAULT_DEPLOYMENT_TOKEN,
+                "X-Vault-Namespace": namespace,
+                "X-Vault-Token": vault_token,
             },
         )
         vault_secret_response = urllib.request.urlopen(req)
+        return json.loads(vault_secret_response.read().decode("utf-8"))["data"]["data"]
+    except json.JSONDecodeError as e:
+        raise Exception("Error decoding Vault secret from response")
     except Exception as e:
-        raise Exception(f"Attempt to reach Vault failed with error: {e}")
-
-    if vault_secret_response.getcode() != 200:
-        raise Exception(
-            f"Request to Vault failed with status code {vault_secret_response.getcode()}"
-        )
-
-    try:
-        payload = json.loads(vault_secret_response.read().decode("utf-8"))["data"]["data"]
-    except Exception as e:
-        raise Exception(f"Failed to parse Vault payload with error: {e}")
-
-    if not sys.stdout.isatty():
-        print('\n'.join(f"{key}={value}" for key, value in payload.items()))
+        raise Exception(f"Error fetching Vault secret: {e}")
 
 if __name__ == "__main__":
     main()
