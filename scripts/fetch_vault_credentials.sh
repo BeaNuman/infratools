c138790503ef3854e0321740703bad2332aec984
Alex Thorley <alex.thorley@numan.com>
2022-08-05 10:35:22 +0100

    Formatting and re-ordering

--- scripts/fetch_vault_credentials.sh
+++ scripts/fetch_vault_credentials.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set -euo pipefail
+
+SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PYTHON_SCRIPT_OUTPUT="$($SCRIPTPATH/fetch_vault_credentials.py)"
 eval "$PYTHON_SCRIPT_OUTPUT"
