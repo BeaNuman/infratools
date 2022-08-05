93f3538c0efd670e8574a4ffd255eff64ff3f125
Alex Thorley <100134391+alex-thorley-numan@users.noreply.github.com>
2022-08-05 10:31:14 +0100

    Update scripts/fetch_vault_credentials.sh
    Use shell options to ensure script fails when it needs to
    
    Co-authored-by: Andy Paine <109977365+andy-paine-numan@users.noreply.github.com>

--- scripts/fetch_vault_credentials.sh
+++ scripts/fetch_vault_credentials.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-PYTHON_SCRIPT_OUTPUT=$($SCRIPTPATH/fetch_vault_credentials.py)
-PYTHON_SCRIPT_EXIT_CODE=$?
-
-if [ ${PYTHON_SCRIPT_EXIT_CODE} == 0 ]; then
-    eval "${PYTHON_SCRIPT_OUTPUT}"
-else
-    exit ${PYTHON_SCRIPT_EXIT_CODE}
-fi
+set -euo pipefail
+PYTHON_SCRIPT_OUTPUT="$($SCRIPTPATH/fetch_vault_credentials.py)"
+eval "$PYTHON_SCRIPT_OUTPUT"
