d3848ae090a1aa49fdc9285aed739d3b6bb168e5
Alex Thorley <alex.thorley@numan.com>
2022-08-09 15:49:55 +0100

    Revert changes to shell options in scripts

--- scripts/fetch_vault_credentials.sh
+++ scripts/fetch_vault_credentials.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PYTHON_SCRIPT_OUTPUT="$($SCRIPTPATH/fetch_vault_credentials.py)"
-eval "$PYTHON_SCRIPT_OUTPUT"
+
+PYTHON_SCRIPT_OUTPUT=$($SCRIPTPATH/fetch_vault_credentials.py)
+PYTHON_SCRIPT_EXIT_CODE=$?
+
+if [ ${PYTHON_SCRIPT_EXIT_CODE} == 0 ]; then
+    eval "${PYTHON_SCRIPT_OUTPUT}"
+else
+    exit ${PYTHON_SCRIPT_EXIT_CODE}
+fi
